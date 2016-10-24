329988ff329e3b3e0ff239962e40c524eceefa73
Manuel <salomanuel.aux+code@gmail.com>
2016-10-24 21:40:09 +0200

    begin ZvKemp tutorial

---
+++ exercises/BinaryTrees-ZvKemp.rb
@@ -0,0 +1,15 @@
+module BinaryTree
+	class Node
+		attr_reader 	:value
+		attr_accessor :left, :right
+
+		def initialize(v)
+			@value = v
+		end
+	end
+end
+
+tree = BinaryTree::Node.new(10)
+tree.left = BinaryTree::Node.new(5)
+tree.right = BinaryTree::Node.new(15)
+puts tree.inspect
