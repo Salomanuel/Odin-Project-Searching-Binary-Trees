236d85a6c3e986f302c4e19a6ec9ad3daff36778
Manuel <salomanuel.aux+code@gmail.com>
2016-10-24 23:49:02 +0200

    add insert method

--- exercises/BinaryTrees-ZvKemp.rb
+++ exercises/BinaryTrees-ZvKemp.rb
@@ -7,8 +7,34 @@ module BinaryTree
 			@value = v
 		end
 
+		def insert(v)
+			case (value <=> v)
+			when  1 then insert_left(v)
+			when -1 then insert_right(v)
+			when  0 then false #the value is already there
+			end
+		end
+
+
 		def inspect
 			"{#{value}::#{left.inspect}|#{right.inspect}}"
+		end
+
+		private
+		def insert_left(v)
+			if left
+				left.insert(v)
+			else
+				self.left = Node.new(v)
+			end
+		end
+
+		def insert_right(v)
+			if right
+				right.insert(v)
+			else
+				self.right = Node.new(v)
+			end
 		end
 	end
 end
@@ -16,4 +42,6 @@ end
 tree = BinaryTree::Node.new(10)
 tree.left = BinaryTree::Node.new(5)
 tree.right = BinaryTree::Node.new(15)
+puts tree.inspect
+tree.insert(3)
 puts tree.inspect
