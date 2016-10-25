d1d2e8521745e71370f294293b3956ea2571051d
Manuel <salomanuel.aux+code@gmail.com>
2016-10-25 11:59:28 +0200

    start Mike Per.. tutorial

---
+++ exercises/BinaryTree-MikePer.rb
@@ -0,0 +1,20 @@
+class Node
+	include Enumerable
+	attr_accessor :data, :left, :right
+	def initialize(data)
+		@data = data
+	end
+
+	def each(&block)
+		block.call(self)
+		left.each(&block) if left
+		right.each(&block) if right
+	end
+end
+
+root       = Node.new(7)
+root.left  = Node.new(3)
+root.right = Node.new(12)
+root.each { |x| puts x.data }
+
+# puts root.inject(0) { |memo, node| memo += node.data }
