bf5c050b367a98d4f3ab65c58b58e5fd256c0d5d
Manuel <salomanuel.aux+code@gmail.com>
2016-10-28 19:04:40 +0200

    done with the third analysis of rubyalgorithms take on trees

--- exercises/rubyalgo-binary-tree3.rb
+++ exercises/rubyalgo-binary-tree3.rb
@@ -56,21 +56,56 @@ class BinarySearchTree
 		post_order(node.right, &block)
 		yield node
 	end
+
+	def search(key, node=@root)
+		return false if node.nil?
+		if key < node.key
+			search(key, node.left)
+		elsif key > node.key
+			search(key, node.right)
+		else
+			return node
+		end
+	end
+
+	def check_height(node)
+		return 0 if node.nil?
+		
+		leftHeight = check_height(node.left)
+		return -1 if leftHeight == -1
+
+		rightHeight = check_height(node.right)
+		return -1 if rightHeight == -1
+
+		diff = leftHeight - rightHeight
+		if diff.abs > 1
+			-1
+		else
+			[leftHeight, rightHeight].max + 1
+		end
+	end
+
+	def is_balanced?(node=@root)
+		check_height(node) == -1 ? false : true
+	end
 end
 
 tree = BinarySearchTree.new
 # 6.times { |n| tree.insert(n) }
+# nodoOtto = tree.insert(8)
 
 randi = [4, 18, 2, 5, 7, 16, 19, 10, 0, 6]
 randi.each { |n| tree.insert(n) }
 
-puts tree.in_order { |n| print n.key.to_s + " " }
+# puts tree.in_order { |n| print n.key.to_s + " " }
+# puts "*"*30
+# puts tree.pre_order { |n| print n.key.to_s + " " }
+# puts "*"*30
+# puts tree.post_order { |n| print n.key.to_s + " " }
 
-puts "*"*30
+# puts tree.search(10).class
+# puts tree.search(11)
 
-puts tree.pre_order { |n| print n.key.to_s + " " }
+puts "is the tree balanced? #{tree.is_balanced?}"
 
-puts "*"*30
-
-puts tree.post_order { |n| print n.key.to_s + " " }
-
+# puts tree.is_balanced(nodoOtto)
