a2d2e150fe018515fc8a990305fae570500b7588
Manuel <salomanuel.aux+code@gmail.com>
2016-10-28 18:26:09 +0200

    add in, pre and post order search methods

--- exercises/rubyalgo-binary-tree3.rb
+++ exercises/rubyalgo-binary-tree3.rb
@@ -24,9 +24,53 @@ class BinarySearchTree
 			end
 		end
 	end
+	def initialize
+		@root = nil
+	end
+
+	def insert(key)
+		if @root.nil?
+			@root = Node.new(key)
+		else
+			@root.insert(key)
+		end
+	end
+
+	def in_order(node=@root, &block)
+		return if node.nil?
+		in_order(node.left,  &block)
+		yield node
+		in_order(node.right, &block)
+	end
+
+	def pre_order(node=@root, &block)
+		return if node.nil?
+		yield node
+		pre_order(node.left,  &block)
+		pre_order(node.right, &block)
+	end
+
+	def post_order(node=@root, &block)
+		return if node.nil?
+		post_order(node.left,  &block)
+		post_order(node.right, &block)
+		yield node
+	end
 end
 
+tree = BinarySearchTree.new
+# 6.times { |n| tree.insert(n) }
 
-	 10
-3		  15
-	   12 19
+randi = [4, 18, 2, 5, 7, 16, 19, 10, 0, 6]
+randi.each { |n| tree.insert(n) }
+
+puts tree.in_order { |n| print n.key.to_s + " " }
+
+puts "*"*30
+
+puts tree.pre_order { |n| print n.key.to_s + " " }
+
+puts "*"*30
+
+puts tree.post_order { |n| print n.key.to_s + " " }
+
